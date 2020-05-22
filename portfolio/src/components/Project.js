baa951edbb5fa8a760f9e69fcc45ac7f2b1ee8f6
Ricardo Russ <ricardoruss@live.com>
2020-05-22 00:12:07 -0400

    code cleanup, rounding edges

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Project = (props) => {
     return (
         <div className="col-sm-6 col-md-12 col-lg-6">
-        <img className="img-fluid port-image" src={props.src} alt={props.alt}/>
+        <img className="img-fluid port-image" src={props.src} alt={props.name}/>
         <p>
-        <a href={props.deployURL}target="_blank">{props.name}</a></p>
+        <a href={props.deployURL} target="_blank">{props.name}</a></p>
         <p><a href={props.repoURL} target="_blank">{props.name} Repository</a></p>
       </div>
       );
--- portfolio/src/pages/Portfolio.js
+++ portfolio/src/pages/Portfolio.js
@@ -10,33 +10,32 @@ const Portfolio = () => {
         <article className="block">
           <h1 className="block-header">Portfolio</h1>
           <div class="row">
-        <Project name="Brew2You" 
-        src="assets/imgs/ProjectFunction.png"
-         deployURL="https://ricardolrjr.github.io/Brew-2-You/" 
-          repoURL="https://github.com/RicardoLRjr/Brew-2-You" />
+          <Project name="FriendlyFire" 
+        src="assets/imgs/FFscreenshot.png"
+         deployURL="https://secure-tor-59417.herokuapp.com/" 
+          repoURL="https://github.com/RicardoLRjr/FriendlyFire" />
         <Project
         name="PokeTeam Manager"
         src="assets/imgs/PokeTeamManager.png"
         deployURL="https://game-related-app.herokuapp.com"
         repoURL="https://github.com/RicardoLRjr/Project-2-working-title-"/>
         </div>
-        <div class="row">
+        <div class="row"> 
+        <Project name="Brew2You" 
+        src="assets/imgs/ProjectFunction.png"
+         deployURL="https://ricardolrjr.github.io/Brew-2-You/" 
+          repoURL="https://github.com/RicardoLRjr/Brew-2-You" />
         <Project
         name="Eat the Burger"
         src="assets/imgs/EatDaBurgerReadme.gif"
         deployURL="https://evening-shelf-95210.herokuapp.com"
         repoURL="https://github.com/RicardoLRjr/Eat-the-burger"/>
-        <Project
+        </div>
+        <div class="row">
+          <Project
         name="Employee Management System"
         src="assets/imgs/EmployeeManagerDemonstration.gif"
         repoURL="https://github.com/RicardoLRjr/SQL-Employee-Tracker"/>
-        </div>
-        <div class="row">
-        <Project
-        name="Refactoring Assignment"
-        src="assets/imgs/Refactoring.png"
-        deployURL="https://ricardolrjr.github.io/Homework-for-28-2-2020/"
-        repoURL="https://github.com/RicardoLRjr/Homework-for-28-2-2020"/>
         <Project
         name="Day Planner"
         src="assets/imgs/DayPlanner.png"
